ddac02a9f1e19e93e3c83dcf6868ab6ccceaaad3
OctaFusari <fusarioctavian@gmail.com>
2024-11-27 12:52:00 +0100

    modifiche stampa degli stati

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,43 @@
 <template>
   <main>
-    <MyNavbar/>
-    
-  <v-container fluid>
-    <!-- Barra di ricerca sopra l'immagine di sfondo -->
-    <v-row justify="center" class="search-bar">
-      <v-col cols="12" md="4">
-        
-      <v-text-field
-        append-inner-icon="mdi-magnify"
-        density="compact"
-        label="Cerca una ricetta..."
-        variant="solo"
-        hide-details
-        single-line
-      ></v-text-field>
-      </v-col>
-    </v-row>
-  </v-container>
-  <div>
-    <div class="container__ricerca" v-for="recipe in recipes">
-      <h1>{{ recipe.food.calories }}</h1>
-    </div>
-  </div>
-    <div>
-      <a>ciao: {{ recipes }}</a>
+    <MyNavbar />
+
+    <v-container fluid>
+      <!-- Barra di ricerca sopra l'immagine di sfondo -->
+      <v-row justify="center" class="search-bar">
+        <v-col cols="12" md="4">
+          <v-text-field
+            append-inner-icon="mdi-magnify"
+            density="compact"
+            label="Cerca una ricetta..."
+            variant="solo"
+            hide-details
+            single-line
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="container__ricerca">
+      <div v-for="recipe in recipes"><!-- 
+        <v-card class="mx-auto" max-width="344">
+          <v-img height="200px" v-bind:src="recipe.recipe.image" cover></v-img>
+
+          <v-card-title>
+            <h1>{{ recipe.recipe.cuisineType[0] }}</h1>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn color="orange-lighten-2" text="Esplora"></v-btn>
+          </v-card-actions>
+        </v-card> -->
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-  import MyNavbar from "../components/navbar.vue";
-  import axios from "axios";
+import MyNavbar from "../components/navbar.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
@@ -40,37 +46,46 @@ export default {
   },
   data() {
     return {
-      searchQuery: '',
-      recipes: null,
+      searchQuery: "",
+      recipes:[{}],
       error: null,
     };
   },
   methods: {
     fetchRecipes() {
+
+      let ArrayStati = ["American", "Italian", "Mexican"]
+
+      for(let i = 0; i < ArrayStati.length; i++){
       axios
-        .get('https://api.edamam.com/api/recipes/v2', {
+        .get("https://api.edamam.com/api/recipes/v2", {
           params: {
-            type: 'public',
+            type: "public",
             beta: true,
-            app_id: 'fed994cd',
-            app_key: 'af0e83d2d10db0905af45dcedcba81f0',
-            cuisineType: 'American',
-              random: true,
+            app_id: "fed994cd",
+            app_key: "af0e83d2d10db0905af45dcedcba81f0",
+            cuisineType: ArrayStati[i],
+            random: true,
           },
           headers: {
-            'accept': 'application/json',
-            'Edamam-Account-User': 'Giada',
-            'Accept-Language': 'en',
+            accept: "application/json",
+            "Edamam-Account-User": "Giada",
+            "Accept-Language": "en",
           },
         })
-        .then(response => {
-          this.recipes = response.data.hits.map((hit:any) => hit.recipe);
+        .then((response) => {
+
+          let oggettoMomentaneo = JSON.parse(JSON.stringify(response.data.hits[0]))
+          this.recipes.push(JSON.parse(JSON.stringify(oggettoMomentaneo.recipe)));
+          console.log(oggettoMomentaneo);
           console.log(this.recipes);
         })
-        .catch(error => {
+        .catch((error) => {
           this.error = error;
-          console.error('Error fetching recipes:', error);
+          console.error("Error fetching recipes:", error);
         });
+
+      }
     },
   },
   mounted() {
@@ -88,7 +103,7 @@ export default {
 }
 
 .v-container {
-  background-image: url('../assets/International-Recipes-1170x780.jpg');
+  background-image: url("../assets/International-Recipes-1170x780.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -99,6 +114,23 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
+.container__ricerca {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: ". . .";
+  margin: 4vh 8vw 1vh 8vw;
+}
+
+.container__ricerca div {
+  margin: 1vh;
+}
+
+@media only screen and (max-width: 800px) {
+  .container__ricerca {
+    grid-template-columns: 1fr;
+    grid-template-areas: ".";
+  }
+}
 </style>
-
-
